67926f03824862b11aab132a24c452612591886d
Camila Ribeiro de Moraes <camilaribeiro0308@gmail.com>
2021-10-27 21:40:46 -0300

    Adicionando uma pequena explicação do código

--- Download de Video/download.py
+++ Download de Video/download.py
@@ -6,20 +6,20 @@ from PySimpleGUI import PySimpleGUI as sg #Interface gráfica
 
 #Layout
 sg.theme('Reddit') 
-layout = [
-    [sg.Text('Link'),sg.Input(key ='link')],
+layout = [ #o que terá na janela, local de texto para input, botão de download
+    [sg.Text('Link'),sg.Input(key ='link')],        
     [sg.Button('Download')]
 ]
 #Janela
-janela = sg.Window('Baixar vídeo',layout)
+janela = sg.Window('Baixar vídeo',layout) #Título da janela e o layout
 #Ler Eventos
 while True:
     eventos,valores = janela.read()
-    if eventos == sg.WINDOW_CLOSED: 
+    if eventos == sg.WINDOW_CLOSED: #Se clicar no botão de fechar, sai da GUI
         break
     
     if eventos == 'Download':
-        x = str(valores.values())
+        x = str(valores.values()) #como se trata de um dicionário, devo mostrar os valores do mesmo
         YouTube(x).streams.get_highest_resolution().download()
         
         
